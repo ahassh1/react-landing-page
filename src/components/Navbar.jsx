1146c229449aa64ed14800220f1df8cd5a0f8ce4
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-10-25 13:26:22 +0600

    shopping cart done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router";
+import { FaShoppingCart } from "react-icons/fa";
 
 const navItems = [
   { path: "/", label: "Furniture" },
@@ -16,7 +17,7 @@ const NavItems = () => {
           <NavLink
             to={item.path}
             className={({ isActive }) =>
-              isActive ? "text-orange-300 font-bold" : "hover:text-orange-500"
+              isActive ? "text-orange-400 font-bold" : "hover:text-orange-500"
             }
           >
             {item.label}
@@ -34,11 +35,16 @@ const Navbar = () => {
         {/* logo */}
         <Link href="logo">logo</Link>
         {/* desktop menu itmes  */}
-        <div>
+        <div className="hidden md:block">
           <NavItems />
         </div>
         {/* cart */}
-        <div>cart</div>
+        <div className="hidden md:block cursor-pointer relative">
+          <FaShoppingCart className="text-2xl" />
+          <sup className="absolute top-0 -right-3 bg-orange-300 text-white w-5 h-5 rounded-full flex items-center justify-center text-xs">
+            0
+          </sup>
+        </div>
       </nav>
     </header>
   );
