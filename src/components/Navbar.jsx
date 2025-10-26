b5e35882b4511ff5e2b1a0790dcd89854ba99bfc
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-10-26 08:27:15 +0600

    hamburger done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, NavLink } from "react-router";
 import { FaShoppingCart } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { FaRegTimesCircle } from "react-icons/fa";
 
 const navItems = [
   { path: "/", label: "Furniture" },
@@ -10,11 +11,11 @@ const navItems = [
   { path: "/contact", label: "Contact" },
 ];
 
-const NavItems = () => {
+const NavItems = ({ toggleMenu }) => {
   return (
     <ul className="flex flex-col md:flex-row items-center md:space-x-8 gap-6">
       {navItems.map((item, index) => (
-        <li key={index}>
+        <li key={index} onClick={toggleMenu}>
           <NavLink
             to={item.path}
             className={({ isActive }) =>
@@ -39,24 +40,45 @@ const Navbar = () => {
     <header>
       <nav className="flex justify-between items-center py-6 px-4 container mx-auto max-w-screen-2xl">
         {/* logo */}
-        <Link href="logo">logo</Link>
+        <Link to="/" className="font-bold hover:text-orange-300">
+          FurnitureEnd
+        </Link>
 
         {/* hamburger menu for mobile device */}
         <div
           onClick={toggleMenu}
-          className="md:hidder cursor-pointer hover:text-orange-300"
+          className="md:hidden cursor-pointer hover:text-orange-300"
         >
-          {isMenuOpen ? <p>X</p> : <GiHamburgerMenu />}
+          {isMenuOpen ? null : <GiHamburgerMenu />}
         </div>
 
         {/* desktop menu itmes  */}
         <div className="hidden md:block">
           <NavItems />
         </div>
+
+        {/* mobile menu items  */}
+        <div
+          className={`fixed top-0 left-0 w-full h-screen bg-black bg-opacity-80 flex flex-col items-center justify-center space-y-8 text-white transition-transform transform ${
+            isMenuOpen ? "translate-x-0" : "-translate-x-full"
+          } md:hidden`}
+        >
+          <div
+            className="cursor-pointer absolute top-4 right-4"
+            onClick={toggleMenu}
+          >
+            <FaRegTimesCircle className="text-2xl" />
+          </div>
+          <NavItems toggleMenu={toggleMenu} />
+        </div>
+
         {/* cart */}
         <div className="hidden md:block cursor-pointer relative">
           <FaShoppingCart className="text-2xl" />
-          <sup className="absolute top-0 -right-3 bg-orange-300 text-white w-5 h-5 rounded-full flex items-center justify-center text-xs">
+          <sup
+            className="absolute top-0 -right-3 bg-orange-300 text-white w-5 h-5
+           rounded-full flex items-center justify-center text-xs"
+          >
             0
           </sup>
         </div>
@@ -64,5 +86,4 @@ const Navbar = () => {
     </header>
   );
 };
-
 export default Navbar;
