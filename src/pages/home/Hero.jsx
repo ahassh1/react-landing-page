d1f7d1dbc2d02765fb56c18ba4f5552303b36f12
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-10-26 10:12:24 +0600

    banner done

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -3,10 +3,23 @@ import bannerImg from "../../assets/banner.png";
 
 const Hero = () => {
   return (
-    <div
+    <section
       className="h-screen relative bg-cover bg-center text-white"
       style={{ backgroundImage: `url(${bannerImg})` }}
-    ></div>
+    >
+      <div
+        className="text-center md:pt-48 pt-25 space-y-6 md:w-1/2 mx-auto
+      "
+      >
+        <h1 className="text-3xl md:text-5xl font-medium lg:leading-tight leading-snug px-2 text-gray-50">
+          Make your interior more minimalistic & modern
+        </h1>
+        <p className="text-xs font-normal w-1/2 mx-auto text-gray-200">
+          Turn your room with panto into a lot more minimalist and modern with
+          ease and speed
+        </p>
+      </div>
+    </section>
   );
 };
 
