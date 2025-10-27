086fd765867b8043861a4034ad4a289b11a61eab
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-10-27 16:48:39 +0600

    product start

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import bannerImg from "../../assets/banner.png";
+import Products from "./Products";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <section className="min-h-screen">
+      {/* banner  */}
+      <div
+        className="w-full h-[400px] bg-cover bg-center flex
+        justify-center items-center text-white"
+        style={{ backgroundImage: `url(${bannerImg})` }}
+      >
+        <h1 className="text-4xl font-bold">Shop Our Product</h1>
+      </div>
+      <Products />
+    </section>
+  );
 };
 
 export default Shop;
